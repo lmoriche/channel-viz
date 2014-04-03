17117842430ef61138830dfceb5ca957131751ab
Laurent Morichetti <l_m@pacbell.net>
2014-04-03 11:27:57 -0700

    sync

--- follows.js
+++ follows.js
@@ -14,7 +14,7 @@
 
 	var defaultKey		= 'lW0rsHCy0OVpLBwvuD0CcN6YovDzBtItCenRJYLkmf7m77Is', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [1157407060], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'Laurent\'s Temperature Controller', // Replaces Xively logo in the header
+		applicationName	= 'RaspberryPi Temperature Controller', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
